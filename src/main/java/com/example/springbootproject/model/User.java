c0ff62cbb83968a85e55294e6a73a76cd0d5d22f
Ion <ion.toncu@student.upt.ro>
2021-11-05 16:44:40 +0200

    Hello world project

--- src/main/java/com/example/springbootproject/model/User.java
+++ src/main/java/com/example/springbootproject/model/User.java
@@ -1,17 +1,38 @@
 package com.example.springbootproject.model;
 
-import lombok.Getter;
-import lombok.Setter;
+import lombok.*;
+import org.springframework.boot.context.properties.ConstructorBinding;
+import org.springframework.stereotype.Component;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
-@Entity
-@Getter
+@Data
+@Builder
 @Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Component
 public class User {
     @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
     private String name;
+    private int age;
 
+    public User(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", age=" + age +
+                '}';
+    }
 }
--- src/main/java/com/example/springbootproject/repo/UserRepository.java
+++ src/main/java/com/example/springbootproject/repo/UserRepository.java
@@ -1,9 +1,13 @@
-package com.example.springbootproject.repository;
+package com.example.springbootproject.repo;
 
 import com.example.springbootproject.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
+       List<User> getAllBy();
+//    User getUserById(Long id);
 }
